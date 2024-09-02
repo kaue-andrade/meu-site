6333a90827c0d495abe01ff4540f03fc0643b301
Kauê Andrade <91803383+kaue-andrade@users.noreply.github.com>
2024-09-02 14:49:49 -0300

    meu-site

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-document.getElementById('form_contato').addEventListener('submit', function(event){
-    event.preventDefault();
+document.getElementById('form_contato').addEventListener('submit', function(event){ // Define que o formulário irá ser enviado
+// e as "funções" para que todo o processo ocorra normalmente. 
 
-    document.getElementById('envioForm').textContent = 'Formulário enviado com sucesso!';
+    event.preventDefault(); // Cancela o comportamento padrão de um evento
 
-    document.getElementById('form_contato').reset();
+    document.getElementById('envioForm').textContent = 'Formulário enviado com sucesso!'; //Coleta o elemento e define 
+    // o conteúdo que irá aparecer 
+
+    document.getElementById('form_contato').reset(); // Reseta o formulário
 
 
 });
